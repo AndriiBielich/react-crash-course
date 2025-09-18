10997925ea075e66e40c37e0b543416b6aff40f9
AndriiBielich <belichandrey89@gmail.com>
2025-09-18 12:07:05 +0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import MyComponent from "./components/MyComponent.tsx";
+
 function App() {
 
     return (
         <>
             <h2>Hello React! Again...</h2>
+            <MyComponent text='Hello Component!'/>
         </>
     )
 }
